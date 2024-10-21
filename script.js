2edf2b112f1b9a630bc761b23e12c79c07f2961f
MuhammadT21 <m.tariq9@share.epsb.ca>
2024-10-21 14:35:01 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// English Idioms
+document.getElementById("1").addEventListener("click", Idiom1);
+function Idiom1() {
+  var output = `bush`;
+  alert(output);
+}
+
+document.getElementById("2").addEventListener("click", Idiom2);
+function Idiom2() {
+  var output = `corners`;
+  alert(output);
+}
+
+document.getElementById("3").addEventListener("click", Idiom3);
+function Idiom3() {
+  var output = `your teeth`;
+  alert(output);
+}
+
+document.getElementById("4").addEventListener("click", Idiom4);
+function Idiom4() {
+  var output = `job`;
+  alert(output);
+}
+
+document.getElementById("5").addEventListener("click", Idiom5);
+function Idiom5() {
+  var output = `one basket`;
+  alert(output);
+}
